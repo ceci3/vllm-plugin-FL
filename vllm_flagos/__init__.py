a73f2277e37a47af4cb5ef5f1d1a84e4942e4e3a
mslv <mslv@baai.ac.cn>
2025-12-03 10:26:36 +0800

    initial commit

---
+++ vllm_flagos/__init__.py
@@ -0,0 +1,9 @@
+def register():
+    """Register the FlagOS platform."""
+
+    return "vllm_flagos.platform.FlagOSPlatform"
+
+
+# def register_connector():
+#     from vllm_ascend.distributed import register_connector
+#     register_connector()
